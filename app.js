218b0a0b5567bfb3335a23181db6410de3936a90
anas abomoch <anas3506606@gmail.com>
2021-01-23 18:54:43 +0200

    first commit

--- app.js
+++ app.js
@@ -24,19 +24,19 @@ app.use("/uploads", express.static("uploads"));
 
 //app.use(express.static('uploads'));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,PATCH"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,PATCH"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   next();
+// });
 app.use('/', indexRouter);
 //app.get("/", (req, res) => res.status(200).json({ message: "Atlantis App" }));
 //app.get("/", (req, res) =>res.render('vindex', { title: 'Express' }));
